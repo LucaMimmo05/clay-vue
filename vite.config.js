e5ac05bc1851d46a03965134e6d54aa8f4ad425b
Matteo Bilotta <me@byloth.net>
2023-03-05 12:20:28 +0100

    [ADD] Added files for a new Vue.js project. ✨

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+import Vue from "@vitejs/plugin-vue";
+
+export default defineConfig({
+  plugins: [Vue()],
+  resolve: {
+    alias: { "@": resolve(__dirname, "src") }
+  }
+});
+
