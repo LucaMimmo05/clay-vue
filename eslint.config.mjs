0a04fdaf59d79e66e9ba4579507f3a32dfecb86a
Matteo Bilotta <me@byloth.dev>
2025-01-31 14:21:43 +0100

    upd: Minor update.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,16 +2,18 @@ import path from "node:path";
 import { fileURLToPath } from "node:url";
 
 import eslintTs from "@byloth/eslint-config-nuxt";
+import eslintStorybook from "eslint-plugin-storybook";
+
 import { includeIgnoreFile } from "@eslint/compat";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const gitignorePath = path.resolve(__dirname, ".gitignore");
 
-export default [includeIgnoreFile(gitignorePath), ...eslintTs, {
-    rules: {
-        "no-trailing-spaces": ["error", { "ignoreComments": true }],
-        "@typescript-eslint/no-non-null-assertion": "off",
-        "@typescript-eslint/unified-signatures": "off"
-    }
+export default [includeIgnoreFile(gitignorePath), ...eslintTs, ...eslintStorybook.configs["flat/recommended"], {
+  rules: {
+    "no-trailing-spaces": ["error", { "ignoreComments": true }],
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/unified-signatures": "off"
+  }
 }];
