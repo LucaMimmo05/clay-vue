e0ddd47e72102a60035b6bd040970c9f1214157b
alb3rt <alberto.cosyanzo05@gmail.com>
2025-06-04 13:08:24 +0200

    Modified shadows and styles of clay-input

--- src/components/ClayInput.vue
+++ src/components/ClayInput.vue
@@ -50,23 +50,26 @@
         --clay-input-color-outline: #{$input-color-outline};
         --clay-input-color-shadow: #{$input-color-shadow};
 
-        --clay-input-spacing-x: 0.5em;
+        --clay-input-spacing-x: 1em;
         --clay-input-spacing-y: 1em;
         --clay-input-spacing: var(--clay-input-spacing-x) var(--clay-input-spacing-y);
 
         --clay-input-roundness: var(--clay-input-spacing-x);
     }
-
     .clay-input {
         border-radius: var(--clay-input-roundness);
         border: none;
         outline: none;
         background-blend-mode: overlay;
-
         padding: var(--clay-input-spacing);
         font-size: 1em;
         position: relative;
+        box-shadow: 0px 2px 6px 0px rgba(0, 0, 0, 0.14) inset;
         background: var(--clay-input-color-background);
+        transition: background-color var(--clay-ease-duration) var(--clay-ease-function),
+                    box-shadow var(--clay-ease-duration) var(--clay-ease-function),
+                    color var(--clay-ease-duration) var(--clay-ease-function),
+                    transform var(--clay-ease-duration) var(--clay-ease-function);
 
         &::before {
             border-radius: var(--clay-input-roundness);
@@ -84,18 +87,6 @@
                 rgba(from var(--clay-input-color-background) r g b / 0.7) 50%,
                 rgba(from var(--clay-input-color-background) r g b / 0.9) 100%
             );
-
-           box-shadow:
-                /* Ombra interna più intensa al focus */
-                inset 0px 3px 10px 0px rgba(0, 0, 0, 0.5),
-                inset 0px -3px 10px 0px rgba(0, 0, 0, 0.5),
-                inset 3px 0px 8px 0px rgba(0, 0, 0, 0.5),
-                inset -3px 0px 8px 0px rgba(0, 0, 0, 0.5),
-                /* Bordo di focus */
-                0px 0px 0px 2px rgba(0, 0, 0, 0.5);
-        }
-
-        &:focus::before {
             box-shadow:
                 /* Ombra interna più intensa al focus */
                 inset 0px 3px 10px 0px rgba(0, 0, 0, 0.5),
@@ -105,8 +96,12 @@
                 /* Bordo di focus */
                 0px 0px 0px 2px rgba(0, 0, 0, 0.5);
         }
-    }
 
+        &:focus-visible {
+            box-shadow: 0px 4px 4px 0px rgba(129, 128, 128, 0.11) inset, 0px 4px 4px 0px rgba(0, 0, 0, 0.34);
+        }
+
+    }
     .clay-input--small {
         --clay-input-spacing-x: 0.375em;
         --clay-input-spacing-y: 0.75em;
