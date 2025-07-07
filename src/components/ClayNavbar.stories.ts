392ddec464a7c5061b59e1d3d591829431c0f7ca
LucaMimmo05 <171295375+LucaMimmo05@users.noreply.github.com>
2025-07-07 09:42:00 +0200

    upd Deleted dfiles from others branch

--- src/components/ClayNavbar.stories.ts
+++
@@ -1,9 +0,0 @@
-import ClayNavbar from "./ClayNavbar.vue";
-export default {
-    title: "ClayNavbar",
-    component: ClayNavbar
-};
-export const Default = () => ({
-    components: { ClayNavbar },
-    template: "<ClayNavbar />"
-});
