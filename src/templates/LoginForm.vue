4cf06c3f796e418736403988066a5c73a4892ff6
Matteo Bilotta <me@byloth.dev>
2025-07-10 12:12:06 +0200

    add: Draft template for `LoginForm`.

---
+++ src/templates/LoginForm.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+    import ClayButton from "@/components/ClayButton.vue";
+    import ClayCard from "@/components/ClayCard.vue";
+    import ClayInput from "@/components/inputs/ClayInput.vue";
+</script>
+
+<template>
+    <ClayCard>
+        <form class="login-form">
+            <ClayInput id="username" />
+            <ClayInput id="password" type="password" />
+            <hr />
+            <ClayButton type="submit">
+                Login
+            </ClayButton>
+        </form>
+    </ClayCard>
+</template>
+
+<style lang="scss">
+    .login-form
+    {
+        display: flex;
+        flex-direction: column;
+        gap: 1em;
+    }
+</style>
